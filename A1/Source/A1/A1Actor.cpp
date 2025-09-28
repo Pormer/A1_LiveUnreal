a7414ffe140db5a44d4faa40fb7766c7c85f14cf
pormer <juwonst@gmail.com>
2025-09-28 17:41:18 +0900

    feat: A1_Actor, Blueprint

---
+++ A1/Source/A1/A1Actor.cpp
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "A1Actor.h"
+#include "A1Object.h"
+#include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AA1Actor::AA1Actor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BodyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BodyMesh"));
+	SetRootComponent(BodyMesh);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> BodyMeshRef(TEXT("/Script/Engine.StaticMesh'/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere'"));
+	if (BodyMeshRef.Succeeded()) {
+		BodyMesh->SetStaticMesh(BodyMeshRef.Object);
+	}
+
+	WingMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WingMesh"));
+	WingMesh->SetupAttachment(BodyMesh);
+	WingMesh->SetRelativeScale3D(FVector(0.5f, 5.0f, 0.2f));
+	WingMesh->SetRelativeLocation(FVector(0.0f, 0.0f, 50.0f));
+
+	if (BodyMeshRef.Succeeded()) {
+		WingMesh->SetStaticMesh(BodyMeshRef.Object);
+	}
+}
+
+// Called when the game starts or when spawned
+void AA1Actor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	TArray<AActor*> Actors;
+	UGameplayStatics::GetAllActorsWithTag(GetWorld(), TEXT("A1Target"), OUT Actors);
+	if (Actors.Num() > 0) {
+		TargetActor = Actors[0];
+	}
+}
+
+// Called every frame
+void AA1Actor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	//FVector NewLocation = Location + FVector::ForwardVector * MovementSpeed * DeltaTime;
+	//SetActorLocation(NewLocation);
+	//AddActorWorldOffset(FVector::ForwardVector * MovementSpeed * DeltaTime);
+
+	if (TargetActor) {
+		FVector Location = GetActorLocation();
+		FVector TargetLocation = TargetActor->GetActorLocation();
+
+		FVector Direction = TargetLocation - Location;
+		AddActorWorldOffset(Direction.GetSafeNormal() * MovementSpeed * DeltaTime);
+	}
+
+	/*FRotator rot = GetActorRotation();
+	FRotator next = FRotator(rot.Pitch, rot.Yaw * RotationRate * DeltaTime, rot.Roll);
+	SetActorRotation(next);*/
+
+
+	AddActorWorldRotation(FRotator(0.0f, RotationRate * DeltaTime, 0.0f));
+
+}
+
