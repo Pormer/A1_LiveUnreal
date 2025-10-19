740f7b1e9c401ab9d57c24d6744b8b5a1ed77148
pormer <juwonst@gmail.com>
2025-10-19 14:46:33 +0900

    feat: A1 pawn blueprint

---
+++ A1/Source/A1/Character/A1Pawn.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/A1Pawn.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+
+// Sets default values
+AA1Pawn::AA1Pawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capusle"));
+	Capsule->InitCapsuleSize(34.f, 100.f);
+	SetRootComponent(Capsule);
+
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(Capsule);
+	Mesh->SetRelativeLocation(FVector(0.f, 0.f, -100.f));
+	Mesh->SetRelativeRotation(FRotator(0.f, -90.f, 0.f));
+
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> MeshAsset(TEXT("/Script/Engine.SkeletalMesh'/Game/_Art/ParagonGreystone/Characters/Heroes/Greystone/Meshes/Greystone.Greystone'"));
+	
+	if (MeshAsset.Succeeded()) {
+		Mesh->SetSkeletalMesh(MeshAsset.Object);
+	}
+}
+
+// Called when the game starts or when spawned
+void AA1Pawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AA1Pawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AA1Pawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
